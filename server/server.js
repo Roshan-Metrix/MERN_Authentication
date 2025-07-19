ebb9292e188e990c3e491cdbca6875c6594e6250
Roshan Patel <roshanpatel12309@gmail.com>
2025-07-19 12:09:44 +0545

    Added routes

--- server/server.js
+++ server/server.js
@@ -1,10 +1,13 @@
 import express from 'express';
 import cors from 'cors';
-import 'dotenv/config';
+import dotenv from 'dotenv'
+dotenv.config();
 import cookieParser from 'cookie-parser';
+import connectDB from './config/mongodb.js';
+
 
 const app = express();
-const port = process.env.PORT | 4000;
+const port = process.env.PORT || 4000;
 
 app.use(express.json());
 app.use(cookieParser());
@@ -13,6 +16,8 @@ app.use(cors({credentials: true}));
 app.get('/',(req,res) => {
     res.send('hello');
 })
+
+connectDB();
 
 app.listen(port,() => {
     console.log(`Server is running at ${port}`)
