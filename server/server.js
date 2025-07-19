cb086a7107aa72cbe20e35e5fad604186f70f5cf
Roshan Patel <roshanpatel12309@gmail.com>
2025-07-19 13:20:37 +0545

    Edited server

--- server/server.js
+++ server/server.js
@@ -3,21 +3,23 @@ import cors from 'cors';
 import dotenv from 'dotenv'
 dotenv.config();
 import cookieParser from 'cookie-parser';
-import connectDB from './config/mongodb.js';
 
+import connectDB from './config/mongodb.js';
+import authRouter from './routes/authRoutes.js'
 
 const app = express();
 const port = process.env.PORT || 4000;
+connectDB();
 
 app.use(express.json());
 app.use(cookieParser());
 app.use(cors({credentials: true}));
 
+// API Endpoints
 app.get('/',(req,res) => {
-    res.send('hello');
+    res.send('API Working');
 })
-
-connectDB();
+app.use('/api/auth',authRouter)
 
 app.listen(port,() => {
     console.log(`Server is running at ${port}`)
