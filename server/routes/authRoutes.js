5f54b2b63bcf9f834d875b9408f1e032f7ab405e
Roshan Patel <roshanpatel12309@gmail.com>
2025-07-27 13:49:11 +0545

    Modified routes

--- server/routes/authRoutes.js
+++ server/routes/authRoutes.js
@@ -1,11 +1,14 @@
 import express from 'express';
-import { registerUser,loginUser,logoutUser } from '../controllers/authController.js';
+import { registerUser,loginUser,logoutUser, sendVerifyOtp, verifyEmail } from '../controllers/authController.js';
+import userAuth from '../middleware/userAuth.js'
 
 const authRouter = express.Router();
 
 authRouter.post('/register', registerUser);
 authRouter.post('/login', loginUser);
 authRouter.post('/logout', logoutUser);
+authRouter.post('/send-verify-otp', userAuth,sendVerifyOtp);
+authRouter.post('/verify-account', userAuth,verifyEmail);
 
 export default authRouter;
 
