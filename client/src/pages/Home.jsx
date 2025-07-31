edf3fe9ece6cefe35bc104f5aaea25a907b59416
Roshan Patel <roshanpatel12309@gmail.com>
2025-07-31 11:02:11 +0545

    Added Header and Navbar

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -5,7 +5,7 @@ import Header from '../components/Header'
 
 const Home = () => {
   return (
-    <div>
+    <div className='flex flex-col items-center justify-center min-h-screen bg-[url("/bg_img.png")] bg-cover bg-center'>
       <NavBar/>
       <Header/>
     </div>
