614bd83e74db84da8c23171e96da7d65bce4b46b
Roshan Patel <roshanpatel12309@gmail.com>
2025-08-03 15:13:29 +0545

    Modified UI for logout

--- client/src/components/NavBar.jsx
+++ client/src/components/NavBar.jsx
@@ -47,10 +47,10 @@ const NavBar = () => {
         </div>
 
         { userData ? 
-        <div className='w-8 h-8 flex justify-center items-center rounded-full bg-black text-white relative group'>
+        <div className='w-11 h-11 flex justify-center items-center rounded-full bg-black text-white relative group'>
          {userData.name[0].toUpperCase()}
          <div className='absolute hidden group-hover:block top-0 right-0 z-19 text-black rounded pt-10'>
-            <ul class='list-none m-0 p-2 bg-gray-100 text-sm'>
+            <ul className='list-none m-1 py-5 px-3 bg-gray-100 text-sm'>
               {!userData.isAccountVerified && 
               <li onClick={sendVerificationOtp} className='py-1 px-2 hover:bg-gray-200 cursor-pointer font-semibold'>Verify email</li> }
               <li onClick={logout} className='py-1 px-2 hover:bg-gray-200 cursor-pointer pr-10 font-semibold'>Logout</li>
@@ -59,7 +59,7 @@ const NavBar = () => {
         </div> : 
         <button 
         onClick={() => navigate('/login')}
-        className='flex items-center gap-2 border border-gray-500 rounded-full px-6 py-2 text-gray-800 hover:bg-gray-100 transition-all'>Login <img src={assets.arrow_icon} alt='' /></button>
+        className='flex items-center gap-2 border border-gray-500 rounded-full px-6 py-2 text-gray-800 hover:bg-gray-100 transition-all cursor-pointer'>Login <img src={assets.arrow_icon} alt='' /></button>
         }
 
 
